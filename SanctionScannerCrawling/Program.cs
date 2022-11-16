cc14cf729856063d33ca066fa257eb3aecce4966
OguzcanAkyuz <103968958+OguzcanAkyuz@users.noreply.github.com>
2022-11-17 00:01:25 +0300

    Add project files.

---
+++ SanctionScannerCrawling/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace SanctionScannerCrawling
+{
+    public class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            
+           
+            string confirm = "Accept: text / html, application / xhtml + xml, */*";
+            string userAgent =  "User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
+            string userAgent2 = "User-Agent: Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0) ";
+            string userAgent3 = "User-Agent:Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +https://www.google.com/bot.html)";
+            string userAgent4 = "User-Agent:Mozilla/5.0 (Windows NT 6.4; WOW64; Trident/7.0; .NET4.0E; .NET4.0C; rv:11.0) like Gecko";
+            var indirilecekDizin = @"C:\htmldowland";
+            string URL = "https://www.sahibinden.com/anasayfa-vitrin?viewType=Gallery";
+            string dosyaAdi = "test.html";
+            string dosyaYolu = @"C:\htmldowland\test.html";
+            int timer = 0;
+            while(timer < 10){ 
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add(confirm);
+            webClient.Headers.Add(userAgent3);
+           
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            webClient.DownloadFile(new Uri(URL), indirilecekDizin + "/" + dosyaAdi);
+                
+            FileStream fs = new FileStream(dosyaYolu, FileMode.Open, FileAccess.Read);
+             fs.Close();
+             RegexWritline.DosyaOku();
+
+             RegexWritline.IsimOku();
+                
+             int milsec = 10000;
+            Thread.Sleep(milsec);
+
+               
+             if (System.IO.File.Exists(dosyaYolu))
+            {
+             System.IO.File.Delete(dosyaYolu);
+            }
+
+            int milseco = 500000;
+            Thread.Sleep(milseco);
+            }
+
+
+        }
+        private static void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+        }
+        private static void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+        }
+
+    }
+}
---
+++ SanctionScannerCrawling/RegexWritline.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SanctionScannerCrawling
+{
+    public class RegexWritline
+    {
+        public static void DosyaOku()
+        {
+            string regexPriceType = "<span>\\s+(?<fiyat>[0-9.]*).*</span></h5>";
+            string testYolu = @"C:\test\test.txt";
+            string dosyaYolu = @"C:\htmldowland\test.html";
+
+            Regex regex = new Regex(regexPriceType, RegexOptions.IgnoreCase);
+            using (var sr = new StreamReader(dosyaYolu))
+            {
+
+                var contents = sr.ReadToEnd();
+                var t = regex.Matches(contents);
+
+                double toplam = 0;
+
+                foreach (Match match in t)
+                {
+                    Console.WriteLine("Fiyatlar: " + match.Groups[1]);
+
+                    toplam += Convert.ToDouble(match.Groups[1].Value);
+
+                    if (File.Exists(dosyaYolu))
+                    {
+                        using (var sw = new StreamWriter(testYolu, true))
+                        {
+
+
+                            sw.WriteLine("FİYAT: " + match.Groups[1]);
+
+                            sw.Close();
+
+
+
+                        }
+                    }
+
+
+                }
+                //ortalama fiyat hesaplama 
+                using (var sw = new StreamWriter(testYolu, true))
+                {
+                    sw.Write("Toplam Fiyat Ortalaması: " + toplam / t.Count);
+                }
+
+                Console.WriteLine("Toplam Fiyat Ortalaması: " + toplam / t.Count);
+
+
+            }
+
+
+
+        }
+        public static void IsimOku()
+        {
+            string testYolu = @"C:\test\test.txt";
+            string dosyaYolu = @"C:\htmldowland\test.html";
+            string regexNameType = "<h2 class=\"item-title\">\\s+(?<title>.*)</h2>";
+            Regex regexString = new Regex(regexNameType, RegexOptions.Multiline);
+            using (var sr = new StreamReader(dosyaYolu))
+            {
+                var contents = sr.ReadToEnd();
+                var nameRegex = regexString.Matches(contents);
+
+                foreach (Match matching in nameRegex)
+                {
+                    Console.WriteLine("İsimler: " + matching.Groups[1]);
+
+                    if (File.Exists(dosyaYolu))
+                    {
+                        using (var streamWriter = new StreamWriter(testYolu, true))
+                        {
+
+
+                            streamWriter.WriteLine("İsimler: " + matching.Groups[1]);
+
+                            streamWriter.Close();
+
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
